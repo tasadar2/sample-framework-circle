cd310c186b493056f2ed7da7ead3b904d5d2d8c3
JCherry <joshcherry@quickenloans.com>
2019-06-21 11:52:05 -0400

    build and run

---
+++ sample-framework-circle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_framework_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
